a8afd32b91fe639d43600bdfd757d1be8f80a46c
bozd4g <bozd4g@gmail.com>
2021-03-14 12:53:15 +0300

    Edit workflow naming and swagger host

--- docs/docs.go
+++ docs/docs.go
@@ -310,7 +310,7 @@ type swaggerInfo struct {
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
 	Version:     "1.0",
-	Host:        "localhost:5000",
+	Host:        "",
 	BasePath:    "/",
 	Schemes:     []string{},
 	Title:       "Comparator API",
